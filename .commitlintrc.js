53ef33e88f9ebcd530fc65380ee5ea52f5ea8ba1
肖学海 <1379228273@qq.com>
2023-09-05 18:28:47 +0800

    feat: 增加lint校验

---
+++ .commitlintrc.js
@@ -0,0 +1,30 @@
+/** @type {import('cz-git').UserConfig} */
+module.exports = {
+  ignores: [commit => commit.includes('init')],
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-leading-blank': [2, 'always'],
+    'footer-leading-blank': [1, 'always'],
+    'header-max-length': [2, 'always', 108],
+    'subject-empty': [2, 'never'],
+    'type-empty': [2, 'never'],
+    'subject-case': [0],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'revert',
+        'chore',
+      ],
+    ],
+  },
+};
--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,25 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    jsxPragma: 'React',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   plugins: ['@typescript-eslint', 'unused-imports', 'import', 'prettier'],
   extends: [
     'eslint:recommended',
     'next',
     'next/core-web-vitals',
     'plugin:@typescript-eslint/recommended',
-    'prettier'
+    'prettier',
+    'plugin:prettier/recommended',
   ],
   rules: {
     // 模块导入顺序规则
@@ -22,13 +31,16 @@ module.exports = {
           {
             pattern: '@/**',
             group: 'external',
-            position: 'after'
-          }
+            position: 'after',
+          },
         ],
-        alphabetize: { order: 'asc', caseInsensitive: false },
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: false,
+        },
         'newlines-between': 'always-and-inside-groups',
-        warnOnUnassignedImports: true
-      }
+        warnOnUnassignedImports: true,
+      },
     ],
     // 自动删除未使用的导入
     // https://github.com/sweepline/eslint-plugin-unused-imports
@@ -38,22 +50,27 @@ module.exports = {
       {
         vars: 'all',
         args: 'none',
-        ignoreRestSiblings: true
-      }
+        ignoreRestSiblings: true,
+      },
     ],
     'import/prefer-default-export': 'off',
     'import/extensions': 'off',
     'import/no-mutable-exports': 'off',
-    'prettier/prettier': 'error',
+    'prettier/prettier': ['error', { trailingComma: 'es5', singleQuote: true }],
     'no-param-reassign': 'off', // 禁止可以修改参数
-    // 'no-var': 'error', // 要求使用 let 或 const 而不是 var
+    'no-var': 'error', // 要求使用 let 或 const 而不是 var
     'no-multiple-empty-lines': ['error', { max: 1 }], // 不允许多个空行
     'no-use-before-define': 'off', // 禁止在 函数/类/变量 定义之前使用它们
-    'prefer-const': 'off', // 此规则旨在标记使用 let 关键字声明但在初始分配后从未重新分配的变量，要求使用 const
     'no-irregular-whitespace': 'off', // 禁止不规则的空白
     '@typescript-eslint/no-unused-vars': 'error', // 禁止定义未使用的变量
     '@typescript-eslint/no-explicit-any': 'off', // 禁止使用 any 类型
     '@typescript-eslint/ban-ts-ignore': 'off', // 禁止使用 @ts-ignore
-    '@typescript-eslint/no-non-null-assertion': 'off' // 不允许使用后缀运算符的非空断言(!)
-  }
+    '@typescript-eslint/no-non-null-assertion': 'off', // 不允许使用后缀运算符的非空断言(!)
+    'react/jsx-curly-spacing': ['error', { when: 'never', children: true }],
+    'react/jsx-props-no-multi-spaces': 'error',
+  },
+  globals: {
+    React: true,
+    JSX: true,
+  },
 };
