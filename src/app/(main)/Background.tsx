ffeab76994603a0ba6467ce035cee019c56a8f86
肖 学海 <1379228273@qq.com>
2023-09-11 23:28:29 +0800

    feat: 增加导航栏动画

--- src/app/(main)/Background.tsx
+++ src/app/(main)/Background.tsx
@@ -1,18 +1,51 @@
 import React from 'react';
 
-import $styles from './background.module.css';
+//  function Background() {
+//   return (
+//     <div className={$styles.background}>
+//       <span className={$styles['background-bottom-mask']}></span>
+
+//       <span className={$styles['background-left']}></span>
+//       <span className={$styles['background-right']}></span>
+//       <div className={$styles.content}>
+//         <div className={$styles.mask}></div>
+//         <div className={$styles['lines-wrapper']}>
+//           <div className={$styles.lines}></div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 
 export default function Background() {
   return (
-    <div className={$styles.background}>
-      <span className={$styles['background-bottom-mask']}></span>
+    <div className='pointer-events-none absolute left-0 top-0 h-full w-full overflow-hidden'>
+      <div
+        className='[--gradient-color-1=rgba(0,0,0,1)] [--gradient-color-2=rgba(0,0,0,0.8)] [--gradient-color-3=rgba(0,0,0,0)] dark:[--gradient-color-1=rgba(255,255,255,1)] dark:[--gradient-color-2=rgba(255,255,255,0.8)] dark:[--gradient-color-3=rgba(255,255,255,0)] absolute z-[-1] h-full w-full [--gradient-stop-1:60%] [--gradient-stop-2:85%] lg:[--gradient-stop-1:50%] lg:[--gradient-stop-2:90%]'
+        style={{
+          background:
+            'linear-gradient(180deg, var(--gradient-color-1) 0%, var(--gradient-color-2) var(--gradient-stop-1), var(--gradient-color-3) var(--gradient-stop-2), 100% transparent)',
+        }}
+      ></div>
+      <span className='landing_leftLights__L8NEZ opacity-50 dark:opacity-100'></span>
+      <span className='landing_rightLights__agcl8 opacity-50 dark:opacity-100'></span>
+      <span className='absolute bottom-0 left-0 h-48 w-full bg-gradient-to-t from-white to-transparent dark:from-black'></span>
 
-      <span className={$styles['background-left']}></span>
-      <span className={$styles['background-right']}></span>
-      <div className={$styles.content}>
-        <div className={$styles.mask}></div>
-        <div className={$styles['lines-wrapper']}>
-          <div className={$styles.lines}></div>
+      <div className='turbohero-background_container__Vu5AA ![perspective:1000px] sm:![perspective:1000px] md:![perspective:1000px] lg:![perspective:1000px]'>
+        <div
+          className='absolute inset-0 z-[100] [--geist-foreground:white] [--gradient-stop-1:0px] [--gradient-stop-2:50%] dark:[--geist-foreground:black]'
+          style={{
+            background:
+              'linear-gradient(to top, rgba(0,0,0,0) 0px, var(--geist-foreground) 50%)',
+          }}
+        ></div>
+        <div
+          className='absolute inset-0'
+          style={{
+            transform: 'rotateX(75deg)',
+          }}
+        >
+          <div className='turbohero-background_lines__UYrFE'></div>
         </div>
       </div>
     </div>
--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,18 +1,16 @@
 import type { PropsWithChildren } from 'react';
 
-import Header from '@/app/(main)/Header';
-import Container from '@/components/Container';
-
 import Background from './Background';
+import Header from './Header';
 export default function MainLayout({ children }: PropsWithChildren) {
   return (
-    <div className='relative'>
+    <div className='relative z-50'>
       <Background />
 
-      <Container className='relative z-50 min-h-screen bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-400/20'>
-        <Header />
-        <main>{children}</main>
-      </Container>
+      {/* <Container className='relative z-50 min-h-screen bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-400/20'> */}
+      <Header />
+      <main>{children}</main>
+      {/* </Container> */}
     </div>
   );
 }
