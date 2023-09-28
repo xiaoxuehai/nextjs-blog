9498c5859c0e2ae113e95ced25455d97471e5d38
肖学海 <1379228273@qq.com>
2023-09-28 17:51:16 +0800

    feat: 增加contentlayer

---
+++ contentlayer.config.ts
@@ -0,0 +1,22 @@
+import { defineDocumentType, makeSource } from 'contentlayer/source-files';
+
+export const Post = defineDocumentType(() => ({
+  name: 'Post',
+  filePathPattern: '**/*.mdx',
+  contentType: 'mdx',
+  fields: {
+    title: { type: 'string', required: true },
+    date: { type: 'date', required: true },
+  },
+  computedFields: {
+    url: {
+      type: 'string',
+      resolve: post => `/${post._raw.flattenedPath}`,
+    },
+  },
+}));
+
+export default makeSource({
+  contentDirPath: './src/contents',
+  documentTypes: [Post],
+});
--- src/app/(main)/blog/page.tsx
+++ src/app/(main)/blog/page.tsx
@@ -1,3 +1,51 @@
+import { compareDesc, format, parseISO } from 'date-fns';
+import Link from 'next/link';
+
+import { allPosts, Post } from 'contentlayer/generated';
+
+function PostCard(post: Post) {
+  return (
+    <div className='mb-8'>
+      <h2 className='mb-1 text-xl'>
+        <Link
+          href={post.url}
+          className='text-blue-700 hover:text-blue-900 dark:text-blue-400'
+        >
+          {post.title}
+        </Link>
+      </h2>
+      <time dateTime={post.date} className='mb-2 block text-xs text-gray-600'>
+        {format(parseISO(post.date), 'LLLL d, yyyy')}
+      </time>
+      <time dateTime={post.date} className='mb-2 block text-xs text-gray-600'>
+        {post.date}
+      </time>
+      {/* <div
+        className='text-sm [&>*:last-child]:mb-0 [&>*]:mb-3'
+        dangerouslySetInnerHTML={{ __html: post.body.html }}
+      /> */}
+    </div>
+  );
+}
+
 export default function Blog() {
-  return <div>博客</div>;
+  console.log(allPosts, 'allPosts');
+
+  const posts = allPosts.sort((a, b) =>
+    compareDesc(new Date(a.date), new Date(b.date)),
+  );
+
+  return (
+    <div className='mx-auto max-w-xl py-8'>
+      <h1 className='mb-8 text-center text-2xl font-black'>
+        Next.js + Contentlayer Example
+      </h1>
+      {posts.map((post, idx) => (
+        <div key={idx}>
+          <PostCard {...post} />
+          <div>{JSON.stringify(post)}</div>
+        </div>
+      ))}
+    </div>
+  );
 }
