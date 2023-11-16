dc85aee58027f487be2ca2af843c53537a05e9d1
肖学海 <1379228273@qq.com>
2023-11-16 18:25:43 +0800

    feat: 新增dialog组件

--- src/app/(main)/(home)/page.tsx
+++ src/app/(main)/(home)/page.tsx
@@ -1,7 +1,15 @@
 import Background from '@/app/(main)/(home)/Background';
 import Container from '@/components/Container';
 
-import Grid from './Grid';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/Dialog';
+
 // import About from '@/contents/about/index.mdx';
 
 export default function Page() {
@@ -9,10 +17,23 @@ export default function Page() {
     <div className='relative z-10'>
       <Background />
       <Container className='relative z-50 px-2'>
-        <Grid />
+        {/* <Grid /> */}
         {/* <div className='prose relative z-20 max-w-7xl dark:prose-invert'>
           <About />
         </div> */}
+
+        <Dialog>
+          <DialogTrigger>Open</DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Are you sure absolutely sure?</DialogTitle>
+              <DialogDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
       </Container>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,9 @@
 import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
+import tailwindcssAnimate from 'tailwindcss-animate';
 const config: Config = {
   darkMode: ['class'],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
@@ -33,6 +33,6 @@ const config: Config = {
       }),
     },
   },
-  plugins: [typography],
+  plugins: [typography, tailwindcssAnimate],
 };
 export default config;
